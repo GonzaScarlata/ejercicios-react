3c4610d0aba34726fefde6991b970d1668648823
Gonzalo Scarlata <gonza.sc.gs@gmail.com>
2021-04-23 21:14:55 -0300

    ejercicio solucionado, states y arrays

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
-import TaskForm from "./components/TaskForm";
+//import TaskForm from "./components/TaskForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColorForm from "./components/ColorForm";
-import CompanyList from "./components/CompanyList";
+//import CompanyList from "./components/CompanyList";
 //import { ListGroup } from "react-bootstrap";
 
 function App() {
     return (
         <div>
             <Header name="My Friend" />
-            {/* <TaskForm />
-            <ColorForm /> */}
-            <CompanyList />
+            <ColorForm />  
+            {/* 
+           
+            <TaskForm />
+            <CompanyList />*/}
+           
             
         </div>
     );
--- src/components/ColorForm.js
+++ src/components/ColorForm.js
@@ -9,7 +9,8 @@ const ColorForm = () => {
     
 
     useEffect(() => {
-        localStorage.setItem("colorsArray",JSON.stringify(colorsArray))
+        localStorage.setItem("colorsArray",JSON.stringify(colorsArray));
+        console.log('ejecutando UseEffect');
     }, [colorsArray])
 
     const handleSubmit = (e) => {
@@ -23,21 +24,24 @@ const ColorForm = () => {
         setColor(color);
     };
 
-    // No estoy pudiendo usar este método para eliminar colores
+    // No estoy pudiendo usar este método para eliminar colores, creo que hay problema en el useEffect
+    // Parece ser que me está modificando el colorsArray aunque yo no quiera... asignarlo a New Array no me sirve
+    // Para solucionarlo tuve que usar el método slice, para que realmente se creara el nuevo array "newArray"
     const deleteColor = (index) => {
-        let newArray = colorsArray;
+        var newArray = colorsArray.slice();
         for (let i = 0; i < colorsArray.length; i++){
             if (i === index) {
                 newArray.splice(i, 1);
-                setColorsArray(newArray);
             }
+            setColorsArray(newArray);
         }
-        console.log(newArray);
+        console.log(colorsArray, 'Colors Array');
+        console.log(newArray, 'New array');
     }
 
     return (
         <div className="container my-5">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={(e)=> handleSubmit(e)}>
                 <label className="text-light" for="formGroupExampleInput">
                     Tableta de colores
                 </label>
@@ -46,7 +50,7 @@ const ColorForm = () => {
                         type="text"
                         class="form-control"
                         placeholder="Escriba un nombre de color en inglés"
-                        onChange={handleChange}
+                        onChange={(e)=>handleChange(e)}
                         value={color}
                     />
                     <button
